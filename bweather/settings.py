a64ee244a68e559e78cb72762471ab898c561f06
Najmul H. Bappy <http.bappy@gmail.com>
2021-07-19 01:26:29 +0600

    final heroku for bweather

--- bweather/settings.py
+++ bweather/settings.py
@@ -125,6 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # new!
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
+    'staticfiles',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # new!
